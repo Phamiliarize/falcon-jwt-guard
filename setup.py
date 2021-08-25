c36bf4b1b53a708fabe88fbb051575b823717f4d
Jesse Pham <10214230+Phamiliarize@users.noreply.github.com>
2021-08-25 23:06:23 +0900

    setup.py was not committed on release->pushing release to 0.0.5

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from distutils.core import setup
 setup(
   name = 'falcon-jwt-guard',
   packages = ['falcon_jwt_guard'],
-  version = '0.0.4',
+  version = '0.0.5',
   license='MIT',
   description = 'Simple JWT Authentication and nothing else for Falcon REST API.',
   author = 'Jesse Pham',
